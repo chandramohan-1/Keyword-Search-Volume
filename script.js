51f5b3aec8797be9eca97084e2bf05191342de3f
chandra mohan <chandu1919043@gmail.com>
2023-08-10 09:06:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,32 @@
-const API_KEY = ''; // Replace with your actual YouTube API key
+document.addEventListener('DOMContentLoaded', function() {
+    const searchButton = document.getElementById('searchButton');
+    const keywordInput = document.getElementById('keywordInput');
+    const searchResult = document.getElementById('searchResult');
+    const spinnerEl = document.getElementById('spinner');
 
-// Function to fetch search results from YouTube API
-function fetchSearchResults(keyword) {
-    console.log('Fetching search results from YouTube API...');
-    return fetch(`https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&q=${encodeURIComponent(keyword)}`)
-        .then(response => response.json())
-        .then(data => data.items)
-        .catch(error => {
-            throw new Error('An error occurred while fetching YouTube data.');
-        });
-}
+    searchButton.addEventListener('click', function() {
+        const keyword = keywordInput.value.trim();
 
-// Function to estimate search volume based on search results
-function estimateSearchVolume(searchResults) {
-    return searchResults.length;
-}
+        if (keyword === '') {
+            searchResult.textContent = 'Please enter a keyword.';
+            return;
+        }
+        spinnerEl.style.display = 'block'; // Show spinner
 
-// Function to calculate percentage deviation
-function calculateDeviation(actual, estimated) {
-    return ((Math.abs(estimated - actual) / actual) * 100).toFixed(2);
-}
+        const apiKey = ''; //
+        const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(keyword)}&key=${apiKey}`;
 
-document.getElementById('searchForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const keyword = document.getElementById('keyword').value;
-    const searchVolumeResult = document.getElementById('searchVolumeResult');
-
-    console.log(`User submitted keyword: ${keyword}`);
-
-    fetchSearchResults(keyword)
-        .then(searchResults => {
-            const estimatedSearchVolume = estimateSearchVolume(searchResults);
-
-            console.log('Search results fetched successfully.');
-            console.log(`Estimated search volume: ${estimatedSearchVolume}`);
-
-            searchVolumeResult.innerHTML = `
-                Estimated search volume for "${keyword}": ${estimatedSearchVolume} searches`;
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            searchVolumeResult.innerHTML = 'An error occurred.';
-        });
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                const searchVolume = data.pageInfo.totalResults;
+                searchResult.textContent = `The keyword "${keyword}" has been searched ${searchVolume} times.`;
+            })
+            .catch(error => {
+                searchResult.textContent = 'An error occurred while fetching data from the API.';
+            })
+            .finally(() => {
+                spinnerEl.style.display = 'none'; // Hide spinner after API call
+            });
+    });
 });
